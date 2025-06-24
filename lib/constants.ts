d7bc7c9ef9c070c2d7fb1a6cf423dbc6eacff737
Travis CI <yadusurya666@gmail.com>
2025-06-24 17:00:31 +0530

    Added colours

--- lib/constants.ts
+++ lib/constants.ts
@@ -2,5 +2,5 @@ export const SENDER_NAME = process.env.SENDER_NAME || 'support'
 export const SENDER_EMAIL = process.env.SENDER_EMAIL || 'onboarding@resend.dev'
 
 export const USER_ROLES = ['Admin', 'User']
-export const COLORS = ['Dream']
+export const COLORS = ['kspyn']
 export const THEMES = ['Light', 'Dark', 'System','Ghibili','Neo','NeoDark','Star','StarDark']
