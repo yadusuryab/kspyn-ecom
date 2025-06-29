bf4d76637cc56cf9c25e01e9d408cc2431ebb778
Travis CI <yadusurya666@gmail.com>
2025-06-29 23:37:27 +0530

    Padding added for mobiles screen

--- app/[locale]/(root)/layout.tsx
+++ app/[locale]/(root)/layout.tsx
@@ -11,7 +11,7 @@ export default async function RootLayout({
   return (
     <div className='flex flex-col min-h-screen overflow-x-hidden'>
       <Header />
-      <main className='flex-1 flex flex-col md:px-[52px]  '>{children}</main>
+      <main className='flex-1 flex flex-col md:px-[52px]  px-[24px]'>{children}</main>
       <Footer />
     </div>
   )
